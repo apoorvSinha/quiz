cd8b58d2ef28588f32b7f832050147f285679b0b
apoorvSInha <apoorvsinha96@gmail.com>
2023-01-22 17:19:54 +0530

    ic

---
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun getName(view: View){
+
+    }
+}
